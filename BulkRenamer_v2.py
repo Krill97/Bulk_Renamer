5ae85422951b4f05ad9f9967d4d97013108862f9
Di Florio <Simon.DiFlorio@apa.com.au>
2021-03-22 15:41:24 +1000

    - removed input checks from readInputCSV - combined number & extension lists

--- BulkRenamer_v2.py
+++ BulkRenamer_v2.py
@@ -6,19 +6,21 @@
 # Date: 5/03/2021
 
 import os
+import sys
 import csv
 import time
-import matplotlib
+import xlsxwriter
+import docx
 import pandas
 
 
-def main():
+def main(argv: enumerate):
     """
     Main Script Function
     """
 
     # "C:\Users\simdif\OneDrive - APA Group\Downloads\Book1.csv"
-    # C:\\Users\\simdif\\OneDrive - APA Group\\Krill49\\Text_Files
+    # C:\Users\simdif\OneDrive - APA Group\Krill49\Text_Files
 
     # print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
     # print("PATH:", os.environ.get('PATH'))
@@ -33,7 +35,15 @@ def main():
     # Prompt user for CSV, and try reading it
     print("1. Input full path to CSV containing document numbers\n" +
           "   (Shift + Right click CSV file in file explorer, select 'Copy as Path' and paste below)")
-    csv_list = readInputCSV()
+
+    # user_input = input("CSV Path: ")
+    print()
+    csv_list = readInputCSV(
+        "C:\\Users\\simdif\\OneDrive - APA Group\\Downloads\\Book1.csv")
+    while isinstance(csv_list, Exception):
+        user_input = input("\nInvalid CSV Path, try again: ")
+        print()
+        csv_list = readInputCSV(user_input)
 
     # Prompt user for target directory and get list of directory contents
     print("2. Input path to target directory, or leave blank to target the script's current directory")
@@ -42,64 +52,62 @@ def main():
     t0 = time.perf_counter()  # start timer
 
     # Get file extensions from CSV and directory, and add full path to every item
-    csv_list_ext = [os.path.splitext(document) for document in csv_list]
-    csv_list = ["{0}\\{1}".format(input_directory, document)
+    csv_list = list(zip(csv_list, [os.path.splitext(document)[1]
+                                   for document in csv_list]))
+    csv_list = [("{0}\\{1}".format(input_directory, document[0]), document[1])
                 for document in csv_list]
 
-    directory_list_ext = [os.path.splitext(
-        document) for document in directory_list]
-    directory_list = ["{0}\\{1}".format(
-        input_directory, document) for document in directory_list]
+    directory_list = list(zip(directory_list, [os.path.splitext(document)[1]
+                                               for document in directory_list]))
+    directory_list = [("{0}\\{1}".format(input_directory, document[0]), document[1])
+                      for document in directory_list]
 
     # iterate through document and directory list, renaming files in target directory
     try:
-        renameDocuments(csv_list, directory_list,
-                        csv_list_ext, directory_list_ext)
+        renameDocuments(csv_list, directory_list)
     except Exception as e:
         print(e)
 
-    t1 = time.perf_counter()
-    print("\nScript took " + "{:.6f}".format(t1 -
-                                             t0) + " seconds to run")  # stop timer
+    t1 = time.perf_counter()  # stop timer
+    print("\nScript took " +
+          "{:.6f}".format(t1 - t0) +
+          " seconds to run")
 
     # exit program prompt
     input("Press enter to exit ")
 
 
-def readInputCSV():
+def readInputCSV(input_CSV: str):
+    # TODO Implement PANDAS variant
     """
     Try reading an input CreateDoc CSV file and return the list of contained document numbers
     If invalid, try again
     """
     return_doc_list = []
-    input_CSV = input("CSV Path: ")
-    print()
+    input_CSV = input_CSV.replace('\"', "")
+    try:
+        with open(input_CSV, "r") as csvFile:
+            csv_reader = csv.reader(csvFile)
+            print("Valid CSV, reading...")
 
-    while True:
-        try:
-            input_CSV = input_CSV.replace('\"', "")
+            for line in csv_reader:
+                if line and line[0] not in return_doc_list:
+                    stripped_line = str.strip(line[0])
+                    if ' ' in stripped_line:
+                        print(stripped_line +
+                              " contains whitespace... skipping")
+                    else:
+                        return_doc_list.append(stripped_line)
+    except Exception as exc:
+        print(exc)
+        return exc
 
-            with open(input_CSV, "r") as csvFile:
-                csv_reader = csv.reader(csvFile)
-
-                for line in csv_reader:
-                    if line and line[0] not in return_doc_list:
-                        stripped_line = str.strip(line[0])
-                        if ' ' in stripped_line:
-                            print(stripped_line +
-                                  " contains whitespace... skipping")
-                        else:
-                            return_doc_list.append(stripped_line)
-
-            print("Successfully read CSV\n")
-            return return_doc_list
-        except Exception as e:
-            print(e)
-            input_CSV = input("\nInvalid CSV, try again: ")
-            print()
+    print("Finished reading CSV\n")
+    return return_doc_list
 
 
 def openInputDir():
+    # TODO Remove input checks from scope
     """
     Try opening a directory and listing its contents
     If invalid, try again
@@ -122,49 +130,50 @@ def openInputDir():
             print()
 
 
-def renameDocuments(number_list, dir_list, number_ext, dir_ext):
+def renameDocuments(number_list: list, dir_list: list):
     """
-    Loop through document and directory list
-    and rename documents accordingly
+    Loop through document and directory list and rename documents accordingly
     """
-    files_renamed = 0
-    dir_renamed_index = [False] * len(dir_list)
+    rename_counter = 0
+    file_renamed = [False] * len(dir_list)
 
-    for i, document in enumerate(number_list):
-        for j, file in enumerate(dir_list):
+    for i, doc_number in enumerate(number_list):
+        for j, initial_file in enumerate(dir_list):
             # avoid making copies of existing document numbers
-            if (document in dir_list):
-                print(str(i) + ". " + os.path.basename(document) +
+            if (doc_number in dir_list):
+                print(str(i) + ". " + os.path.basename(doc_number[0]) +
                       " already exists in directory... skipping")
                 break
             else:
                 # avoid renaming previously written doc numbers and only rename matching extensions
-                if dir_list[j] in number_list or number_ext[i][1] != dir_ext[j][1] or dir_renamed_index[j]:
+                if (dir_list[j][0] in [doc_number[0] for doc_number in number_list]
+                        or number_list[i][1] != dir_list[j][1]
+                        or file_renamed[j]):
                     if j == len(dir_list) - 1:
                         print("Couldn't find suitable file to rename to " +
-                              os.path.basename(document))
+                              os.path.basename(doc_number))
                     else:
                         continue
                 # rename current file to specified document number
                 else:
-                    os.rename(file, document)
-                    files_renamed += 1
-                    dir_renamed_index[j] = True
-                    print(str(i) + ". " + os.path.basename(file) +
-                          " -> " + os.path.basename(document))
+                    os.rename(initial_file[0], doc_number[0])
+                    rename_counter += 1
+                    file_renamed[j] = True
+                    print(str(i) + ". " + os.path.basename(initial_file[0]) +
+                          " -> " + os.path.basename(doc_number[0]))
                     break
 
     # final status message
-    if files_renamed == 1:
-        print("\nRenamed " + str(files_renamed) +
+    if rename_counter == 1:
+        print("\nRenamed " + str(rename_counter) +
               " file out of " + str(len(number_list)))
-    elif files_renamed > 1:
-        print("\nRenamed " + str(files_renamed) +
+    elif rename_counter > 1:
+        print("\nRenamed " + str(rename_counter) +
               " files out of " + str(len(number_list)))
     else:
-        print("Finished running, no files renamed")
+        print("Finished processing, no files renamed")
 
 
 # driver code
 if __name__ == "__main__":
-    main()
+    main(enumerate(sys.argv))
